e82f90093f3ed90e6d7fe8b06516d1d3d533c286
Deepali <joysingal247@gmail.com>
2025-08-21 22:20:46 +0530

    changes in all folders

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://deepali-lms-qgqc.vercel.app/",
+    origin: "https://deepali-lms-qgqc.vercel.app",
     credentials: true,
   })
 );
